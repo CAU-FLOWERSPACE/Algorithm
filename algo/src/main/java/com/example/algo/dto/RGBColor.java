61d8aeee15c2bccf9c158ab76bf24895b96ebe36
ChoiJaeEun <poungki0823@gmail.com>
2021-10-17 01:53:12 +0900

    RGBColor class

---
+++ algo/src/main/java/com/example/algo/dto/RGBColor.java
@@ -0,0 +1,26 @@
+package com.example.algo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RGBColor implements Comparable {
+    private int r;
+    private int g;
+    private int b;
+    private double proportion;
+
+
+    @Override
+    public int compareTo(Object o) {
+        RGBColor rgbColor = (RGBColor) o;
+        if (this.proportion < rgbColor.proportion) {
+            return -1;
+        } else if (this.proportion == rgbColor.proportion) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}

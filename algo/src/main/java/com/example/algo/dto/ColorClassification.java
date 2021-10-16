065105002e3a732e5844ce0f2172fb1357cb0ee8
ChoiJaeEun <poungki0823@gmail.com>
2021-10-17 01:52:36 +0900

    ColorClassification Enum

---
+++ algo/src/main/java/com/example/algo/dto/ColorClassification.java
@@ -0,0 +1,14 @@
+package com.example.algo.dto;
+
+import java.util.List;
+
+public enum ColorClassification {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    PURPLE,
+    PINK,
+    WHITE
+}
